abf90dbb18992c8a1f4c5c90f7d286e1f330da19
Shinaikos <diguin-76@hotmail.com>
2021-05-21 19:52:37 -0300

    contador funcional

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const selector = select => document.querySelector(select);
 
-const date = selector("#date");
-const day = selector("#counter-day");
-const hour = selector("#counter-hour");
-const minute = selector("#counter-minute");
-const seconds = selector("counter-seconds");
-
 selector("#submit").addEventListener("click", () => {
     const primary = selector("#principal");
     const secondary = selector("#secundario");
@@ -23,6 +17,56 @@ selector("#restart").addEventListener("click", () => {
     }
 });
 
-function updateCounter() {
-
+function insertData(day, hour, minute, second) {
+    selector("#counter-day span").innerHTML = day;
+    selector("#counter-hour span").innerHTML = hour;
+    selector("#counter-minute span").innerHTML = minute;
+    selector("#counter-second span").innerHTML = second;
 }
+const getCurrentDate = () => Date.now();
+
+
+const getTargetDate = () => {
+    let getDate = document.querySelector("input").value;
+    let targetDate = new Date(getDate);
+    return targetDate.getTime();
+}
+
+const intervalDate = () => getTargetDate() - getCurrentDate();
+
+function transform() {
+    let interval = parseInt(intervalDate());
+    let seconds = interval / 1000;
+    seconds += 10800;
+    let dias = seconds / 86400;
+    let hours = (seconds % 86400) / 3600;
+    let minutes = (seconds % 3600) / 60;
+    seconds = seconds % 60
+    dias = parseInt(dias);
+    hours = parseInt(hours);
+    minutes = parseInt(minutes);
+    seconds = parseInt(seconds)
+
+    insertData(dias, hours, minutes, seconds)
+}
+
+function start() {
+    getTargetDate();
+    setInterval(transform, 1000)
+}
+
+function restart() {
+    setTimeout(transform)
+    dateMin()
+}
+
+function dateMin() {
+    let date = new Date();
+    let ano = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+
+    let setDate = document.querySelector("input");
+    setDate.setAttribute("min", `${ano}-0${month+1}-${day+1}`);
+}
+dateMin();
